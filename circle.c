322772d016ac41f3668c7794a9594bf013379e94
Peter Laust Almvig <petealmvig@gmail.com>
2022-10-03 11:56:37 +0200

    Added comments to circle

--- circle.c
+++ circle.c
@@ -4,6 +4,7 @@
 
 void fiveCircles(circle c[]) {
 /*post: returns an array with five circles - solution to 6.b*/
+//Loop over fives circles i=1->5 and set their coordinate and radius accordingly.
 	for(int i = 1; i <= 5; i++){
 		c[i].p.x = i;
 		c[i].p.y = i;
@@ -13,6 +14,7 @@ void fiveCircles(circle c[]) {
 
 int circleIsValid(const circle * c) {
 /*post: answer to exercise 6.c*/
+//The circle is valid if it's radius is above 0.
 	if(c->r > 0){
 		return 1;
 	}
